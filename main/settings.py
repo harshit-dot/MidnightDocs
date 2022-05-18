b83001740e7c452ba51d7618d78de2b1a54e619c
harshit-dot <harshit.19B101021@abes.ac.in>
2022-05-18 12:50:17 +0530

    additional files and changes for Heroku

--- main/settings.py
+++ main/settings.py
@@ -171,5 +171,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'midnightsdocs@gmail.com'
-EMAIL_HOST_PASSWORD = 'mbzxtljkjvbuqjbc'
+EMAIL_HOST_USER = 'midnightdocs128@gmail.com'
+EMAIL_HOST_PASSWORD = 'mgjhxjelhvdfvrfi'
+
